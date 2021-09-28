bacea8bc91dc40d5be0ca821ba9a7ad7b606cb87
marcostom32 <62820238+marcostom32@users.noreply.github.com>
2021-09-28 16:16:37 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -4,6 +4,7 @@ local PlayerData = {}
 local training = false
 local resting = false
 local membership = false
+local show = 1
 
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
     PlayerData = QBCore.Functions.GetPlayerData()
@@ -57,46 +58,211 @@ local function FetchSkills()
     end)
 end
 
-RegisterCommand('skills', function() 
+
+function SkillMenu2()
+	ped = PlayerPedId();
+	MenuTitle = "Skills"
+    RefreshSkills()
+	ClearMenu()
     for type, value in pairs(Config.Skills) do
-        TriggerEvent('chat:addMessage', {
-            template = '<div class="chat-message advert"><div class="chat-message-body"><strong>Habs</strong><br>'..type .. ': <span style="color:#0d7ec0">' .. value["Current"] ..'</div></div>',
-        })
+        Menu.addButton(type .. ' <span style="color:#0d7ec0">' .. value["Current"] .. "</span> %", "closeMenuFull",nil)
 	end
+end
+
+function buyMember()
+    TriggerServerEvent('qb-gym:buyMembership')
+    closeMenuFull()
+end
+
+function BuyMembership()
+	ped = PlayerPedId();
+	MenuTitle = "Gym Shop"
+	ClearMenu()
+    Menu.addButton('Buy Membership', "buyMember",nil)
+end
+
+RegisterCommand('habilidades', function() 
+	SkillMenu2()
 end, false)
 
 
-local function UpdateSkill(skill, amount)
+Menu = {}
+Menu.GUI = {}
+Menu.buttonCount = 0
+Menu.selection = 0
+Menu.hidden = true
+MenuTitle = "Menu"
 
-    if not Config.Skills[skill] then
-        print("Skill " .. skill .. " doesn't exist")
-        return
-    end
-    local SkillAmount = Config.Skills[skill]["Current"]
-    if SkillAmount + tonumber(amount) < 0 then
-        Config.Skills[skill]["Current"] = 0
-    elseif SkillAmount + tonumber(amount) > 100 then
-        Config.Skills[skill]["Current"] = 100
-    else
-        Config.Skills[skill]["Current"] = SkillAmount + tonumber(amount)
-    end
-    RefreshSkills()
-    if tonumber(amount) > 0 then
-        QBCore.Functions.Notify('~g~+' .. amount .. '% ~s~' .. skill, 'success')
-    end
-	TriggerServerEvent("qb-skills:update", json.encode(Config.Skills))
+function Menu.addButton(name, func,args,extra,damages,bodydamages,fuel)
+
+	local yoffset = 0.25
+	local xoffset = 0.3
+	local xmin = 0.0
+	local xmax = 0.15
+	local ymin = 0.03
+	local ymax = 0.03
+	Menu.GUI[Menu.buttonCount+1] = {}
+	if extra ~= nil then
+		Menu.GUI[Menu.buttonCount+1]["extra"] = extra
+	end
+	if damages ~= nil then
+		Menu.GUI[Menu.buttonCount+1]["damages"] = damages
+		Menu.GUI[Menu.buttonCount+1]["bodydamages"] = bodydamages
+		Menu.GUI[Menu.buttonCount+1]["fuel"] = fuel
+	end
+
+
+	Menu.GUI[Menu.buttonCount+1]["name"] = name
+	Menu.GUI[Menu.buttonCount+1]["func"] = func
+	Menu.GUI[Menu.buttonCount+1]["args"] = args
+	Menu.GUI[Menu.buttonCount+1]["active"] = false
+	Menu.GUI[Menu.buttonCount+1]["xmin"] = xmin
+	Menu.GUI[Menu.buttonCount+1]["ymin"] = ymin * (Menu.buttonCount + 0.01) +yoffset
+	Menu.GUI[Menu.buttonCount+1]["xmax"] = xmax 
+	Menu.GUI[Menu.buttonCount+1]["ymax"] = ymax 
+	Menu.buttonCount = Menu.buttonCount+1
 end
 
-local function CheckTraining()
-	if resting == true then
-        QBCore.Functions.Notify('You\'re resting', 'primary')
-		resting = false
-		Wait(60000)
-		training = false
+
+function Menu.updateSelection() 
+	if IsControlJustPressed(1, 173) then -- Down Arrow
+		if(Menu.selection < Menu.buttonCount -1 ) then
+			Menu.selection = Menu.selection +1
+		else
+			Menu.selection = 0
+		end		
+		PlaySound(-1, "SELECT", "HUD_FRONTEND_DEFAULT_SOUNDSET", 0, 0, 1)
+	elseif IsControlJustPressed(1, 27) then -- Up Arrow
+		if(Menu.selection > 0)then
+			Menu.selection = Menu.selection -1
+		else
+			Menu.selection = Menu.buttonCount-1
+		end	
+		PlaySound(-1, "SELECT", "HUD_FRONTEND_DEFAULT_SOUNDSET", 0, 0, 1)
+	elseif IsControlJustPressed(1, 215) then
+		MenuCallFunction(Menu.GUI[Menu.selection +1]["func"], Menu.GUI[Menu.selection +1]["args"])
+		PlaySound(-1, "SELECT", "HUD_FRONTEND_DEFAULT_SOUNDSET", 0, 0, 1)
 	end
-	if resting == false then
-        QBCore.Functions.Notify('You can now do exercise again', 'success')
+	local iterator = 0
+	for id, settings in ipairs(Menu.GUI) do
+		Menu.GUI[id]["active"] = false
+		if(iterator == Menu.selection ) then
+			Menu.GUI[iterator +1]["active"] = true
+		end
+		iterator = iterator +1
 	end
+end
+
+function Menu.renderGUI()
+	if not Menu.hidden then
+		Menu.renderButtons()
+		Menu.updateSelection()
+	end
+end
+
+function Menu.renderBox(xMin,xMax,yMin,yMax,color1,color2,color3,color4)
+	DrawRect(0.7, yMin,0.15, yMax-0.002, color1, color2, color3, color4);
+end
+
+function Menu.renderButtons()
+	
+		local yoffset = 0.5
+		local xoffset = 0
+
+		
+		
+	for id, settings in pairs(Menu.GUI) do
+		local screen_w = 0
+		local screen_h = 0
+		screen_w, screen_h =  GetScreenResolution(0, 0)
+		
+		boxColor = {38,38,38,199}
+		local movetext = 0.0
+		if(settings["extra"] == "Garage") then
+			boxColor = {44,100,44,200}
+		elseif (settings["extra"] == "Impounded") then
+			boxColor = {77, 8, 8,155}
+		end
+
+		if(settings["active"]) then
+			boxColor = {31, 116, 207,155}
+		end
+
+
+		if settings["extra"] ~= nil then
+
+			SetTextFont(4)
+
+			SetTextScale(0.34, 0.34)
+			SetTextColour(255, 255, 255, 255)
+			SetTextEntry("STRING") 
+			AddTextComponentString(settings["name"])
+			DrawText(0.63, (settings["ymin"] - 0.012 )) 
+
+			SetTextFont(4)
+			SetTextScale(0.26, 0.26)
+			SetTextColour(255, 255, 255, 255)
+			SetTextEntry("STRING") 
+			AddTextComponentString(settings["extra"])
+			DrawText(0.730 + movetext, (settings["ymin"] - 0.011 )) 
+
+
+			SetTextFont(4)
+			SetTextScale(0.28, 0.28)
+			SetTextColour(11, 11, 11, 255)
+			SetTextEntry("STRING") 
+			AddTextComponentString(settings["damages"])
+			DrawText(0.778, (settings["ymin"] - 0.012 )) 
+
+			SetTextFont(4)
+			SetTextScale(0.28, 0.28)
+			SetTextColour(11, 11, 11, 255)
+			SetTextEntry("STRING") 
+			AddTextComponentString(settings["bodydamages"])
+			DrawText(0.815, (settings["ymin"] - 0.012 ))  
+
+			SetTextFont(4)
+			SetTextScale(0.28, 0.28)
+			SetTextColour(11, 11, 11, 255)
+			SetTextEntry("STRING") 
+			AddTextComponentString(settings["fuel"])
+			DrawText(0.854, (settings["ymin"] - 0.012 )) 
+
+			
+
+			DrawRect(0.832, settings["ymin"], 0.11, settings["ymax"]-0.002, 255,255,255,199)
+			--Global.DrawRect(x, y, width, height, r, g, b, a)
+		else
+			SetTextFont(4)
+			SetTextScale(0.31, 0.31)
+			SetTextColour(255, 255, 255, 255)
+			SetTextCentre(true)
+			SetTextEntry("STRING") 
+			AddTextComponentString(settings["name"])
+			DrawText(0.7, (settings["ymin"] - 0.012 )) 
+
+		end
+		Menu.renderBox(settings["xmin"] ,settings["xmax"], settings["ymin"], settings["ymax"],boxColor[1],boxColor[2],boxColor[3],boxColor[4])
+	 end     
+end
+
+--------------------------------------------------------------------------------------------------------------------
+
+function ClearMenu()
+	--Menu = {}
+	Menu.GUI = {}
+	Menu.buttonCount = 0
+	Menu.selection = 0
+end
+
+function MenuCallFunction(fnc, arg)
+	_G[fnc](arg)
+end
+
+function closeMenuFull()
+    Menu.hidden = true
+    show = nil
+    ClearMenu()
 end
 
 
@@ -147,9 +313,6 @@ CreateThread(function()
 	end
 end)
 
-RegisterCommand('habilidades', function() 
-	SkillMenu()
-end, false)
 	
 CreateThread(function()
 	blip = AddBlipForCoord(-1201.2257, -1568.8670, 4.6101)
@@ -176,6 +339,11 @@ local Shops = {
         coords = vector3(-1195.6551, -1577.7689, 4.631155)
     },
 }
+local checkskills = {
+    [1] = {
+        coords = vector3(-1199.33, -1580.15, 4.61)
+    },
+}
 
 CreateThread(function()
     while true do
@@ -189,11 +357,30 @@ CreateThread(function()
                         sleep = 0
                         DrawText3D(v.coords.x, v.coords.y, v.coords.z, "~b~E~w~ - [Gym Shop]")
                         if IsControlJustReleased(0, 38) then
-                            OpenGymMenu()
+                            BuyMembership()    
+                            Menu.hidden = not Menu.hidden    
                         end
+                        Menu.renderGUI() 
                     end  
                 end
             end
+
+            -- temporal
+            for k, v in pairs(checkskills) do
+                local dist = #(pos - vector3(v.coords.x, v.coords.y, v.coords.z))
+                if dist < 4.5 then
+                    if dist < 1.5 then
+                        sleep = 0
+                        DrawText3D(v.coords.x, v.coords.y, v.coords.z, "~b~E~w~ - [Check Stats]")
+                        if IsControlJustReleased(0, 38) then
+                            SkillMenu2()      
+                            Menu.hidden = not Menu.hidden  
+                        end
+                        Menu.renderGUI() 
+                    end  
+                end
+            end
+
 
             for k, v in pairs(Config.Locations) do
                 local dist = #(pos - vector3(Config.Locations[k].coords.x, Config.Locations[k].coords.y, Config.Locations[k].coords.z))
